b3a95116be216d4c54805f435dcbfe922c171057
Shahar <sharhar@users.noreply.github.com>
2017-07-22 10:27:53 -0700

    True full screen

--- app/src/main/java/pro/shpin/kirill/simplerendering/GameActivity.java
+++ app/src/main/java/pro/shpin/kirill/simplerendering/GameActivity.java
@@ -90,5 +90,12 @@ public class GameActivity extends AppCompatActivity {
 				renderer.useDoublePrecision(isChecked);
 			}
 		});
+		
+		glView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+				| View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+				| View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+				| View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // hide nav bar
+				| View.SYSTEM_UI_FLAG_FULLSCREEN // hide status bar
+				| View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
 	}
 }
