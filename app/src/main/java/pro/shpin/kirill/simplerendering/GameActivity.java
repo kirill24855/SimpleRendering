0629fd72fedc90997e2c6c8735c955e4b9509bcc
kirill.shpin <kirill.shpin@gmail.com>
2017-08-04 15:51:12 -0700

    Minor restructure for readability.

--- app/src/main/java/pro/shpin/kirill/simplerendering/GameActivity.java
+++ app/src/main/java/pro/shpin/kirill/simplerendering/GameActivity.java
@@ -54,58 +54,60 @@ public class GameActivity extends AppCompatActivity {
 		ConstraintLayout layout = (ConstraintLayout) findViewById(R.id.activity_game_overlay);
 		layout.addView(glView, 0);
 
-		{ // Setup overlay
-			// Toggle controls menu
-			final ConstraintLayout controlsMenu = (ConstraintLayout) findViewById(R.id.controlsContainer);
-			controlsMenu.setPadding(0, 0, 0, 0);
-			findViewById(R.id.toggleControls).setOnClickListener(new View.OnClickListener() {
-				@Override
-				public void onClick(View v) {
-					toggleView(controlsMenu);
-				}
-			});
-
-			// Change color scheme button
-			findViewById(R.id.changeColorsButton).setOnClickListener(new View.OnClickListener() {
-				@Override
-				public void onClick(View v) {
-					renderer.changeColorScheme();
-				}
-			});
-
-			// Change iterations
-			final TextView iterationText = (TextView) findViewById(R.id.iterationsText);
-			((SeekBar) findViewById(R.id.iterationSlider)).setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
-				@Override
-				public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-					int iterations = progress;
-					iterationText.setText(getApplicationContext().getString(R.string.iterationsText, iterations));
-					renderer.setIterations(iterations);
-				}
-
-				@Override
-				public void onStartTrackingTouch(SeekBar seekBar) {
-					renderer.setRenderMode(GLESRenderer.RENDER_DOWNSCALE);
-				}
-
-				@Override
-				public void onStopTrackingTouch(SeekBar seekBar) {
-					renderer.setRenderMode(GLESRenderer.RENDER_UPSCALE);
-				}
-			});
-
-			// Toggle double precision
-			final ToggleButton precisionToggle = (ToggleButton) findViewById(R.id.precisionToggle);
-			precisionToggle.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-				@Override
-				public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-					renderer.useDoublePrecision(isChecked);
-				}
-			});
-		}
+		setupOverlay();
 	}
 
-	public void toggleView(View view){
+	private void setupOverlay() {
+		// Toggle controls menu
+		final ConstraintLayout controlsMenu = (ConstraintLayout) findViewById(R.id.controlsContainer);
+		controlsMenu.setPadding(0, 0, 0, 0);
+		findViewById(R.id.toggleControls).setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				toggleView(controlsMenu);
+			}
+		});
+
+		// Change color scheme button
+		findViewById(R.id.changeColorsButton).setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				renderer.changeColorScheme();
+			}
+		});
+
+		// Change iterations
+		final TextView iterationText = (TextView) findViewById(R.id.iterationsText);
+		((SeekBar) findViewById(R.id.iterationSlider)).setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+			@Override
+			public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+				int iterations = progress;
+				iterationText.setText(getApplicationContext().getString(R.string.iterationsText, iterations));
+				renderer.setIterations(iterations);
+			}
+
+			@Override
+			public void onStartTrackingTouch(SeekBar seekBar) {
+				renderer.setRenderMode(GLESRenderer.RENDER_DOWNSCALE);
+			}
+
+			@Override
+			public void onStopTrackingTouch(SeekBar seekBar) {
+				renderer.setRenderMode(GLESRenderer.RENDER_UPSCALE);
+			}
+		});
+
+		// Toggle double precision
+		final ToggleButton precisionToggle = (ToggleButton) findViewById(R.id.precisionToggle);
+		precisionToggle.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+			@Override
+			public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+				renderer.useDoublePrecision(isChecked);
+			}
+		});
+	}
+
+	private void toggleView(View view) {
 		if(view.getVisibility()==View.INVISIBLE) view.setVisibility(View.VISIBLE);
 		else if(view.getVisibility()==View.VISIBLE) view.setVisibility(View.INVISIBLE);
 	}
